5458599d38e028a18e0ffcce7e175b4e1ac8fecb
NandiniAgrawal17 <nandini.agrawal_cs19@gla.ac.in>
2022-05-24 18:06:05 +0530

    created setup for project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Topbar from "./components/topbar/Topbar";
+import Home from "./components/pages/home/Home";
+import Single from "./components/pages/single/Single";
+import Write from "./components/pages/write/Write";
+import Settings from "./components/pages/settings/Setting";
+import Login from "./components/pages/login/Login";
+import Register from "./components/pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+  //user is not logged in 
+  const user = false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Topbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/settings" element={user ? <Settings /> : <Register />} />
+        <Route path="/write" element={user ? <Write /> : <Register />} />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
+    </Router>
   );
 }
 
